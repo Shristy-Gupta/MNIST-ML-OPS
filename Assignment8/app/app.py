919186f853c3dd800df82cab3003aacb35c00a6f
Shristy Gupta <gupta.60@iitj.ac.in>
2021-11-09 01:14:40 +0530

    Assignment8
    A8

--- Assignment8/app/app.py
+++ Assignment8/app/app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/post/<int:post_id>")
+def even_odd(post_id):
+    if(post_id % 2==0):
+        return "<p>Number is EVEN!</p>"
+    else:
+       return "<p>Number is ODD!</p>" 
+    
